67ec3bf53adccc91c8aee2eed55e0848e9f042bf
Jake Rowsell <Jake.Rowsell@bglgroup.co.uk>
2018-11-26 08:47:22 +0000

    Create utility functions

---
+++ exercises/index.js
@@ -0,0 +1,22 @@
+function find(x, list) {
+  
+}
+
+function filter(x, list) {
+
+}
+
+function indexOf(x, list) {
+
+}
+
+function reverse(list) {
+
+}
+
+module.exports = {
+  filter,
+  indexOf,
+  find,
+  reverse,
+}
